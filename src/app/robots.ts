c265a1539d54fcc3dcd0ba9cb92cb01e37661283
Divine Davis <divine.davis@gmail.com>
2026-03-28 17:24:43 +0000

    SEO: Enhance robots.txt with multiple sitemaps and disallow search/api paths

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,11 +2,23 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/api/',
-    },
-    sitemap: 'https://movies.unittap.com/sitemap.xml',
+    rules: [
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+        disallow: ['/api/', '/search/'],
+      },
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/search/'],
+        // Note: crawlDelay is supported by Next.js MetadataRoute.Robots
+      },
+    ],
+    sitemap: [
+      'https://movies.unittap.com/sitemap.xml',
+      'https://movies.unittap.com/video-sitemap.xml',
+      'https://movies.unittap.com/sitemap-index.xml',
+    ],
   };
 }
